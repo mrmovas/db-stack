1294738c8c83fd43838b64540683b222d5f9701d
movas <nikos.gogalis.04@gmail.com>
2026-04-22 14:18:41 +0300

    Implement command handling logic and main execution flow

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+
+import { getCommmand } from "./utils/getCommand";
+
+const main = async (): Promise<void> => {
+    const getCommandResult = getCommmand();
+    if(!getCommandResult.success) {
+        if(getCommandResult.error) console.error(getCommandResult.error)
+        process.exit(1);
+    }
+
+    const { direction, action } = getCommandResult;
+
+
+}
+
+main();
---
+++ src/utils/getCommand.ts
@@ -0,0 +1,78 @@
+type CommandDirection = "migrate" | "backup";
+type CommandAction<P extends CommandDirection> = P extends "migrate"
+	? "up" | "upToLatest" | "down"
+	: "create" | "restore";
+
+type CommandSuccessResult =
+	| {
+			success: true;
+			direction: "migrate";
+			action: CommandAction<"migrate">;
+	  }
+	| {
+			success: true;
+			direction: "backup";
+			action: CommandAction<"backup">;
+	  };
+
+type getCommandResult =
+	| CommandSuccessResult
+	| {
+			success: false;
+			error?: string;
+	  };
+
+export function getCommmand(): getCommandResult {
+	const direction = process.argv[2];
+	const action = process.argv[3];
+
+	switch (direction) {
+		case "help": {
+			console.log(
+				"Usage: `tsx src/index.ts <migrate|backup> <action>`",
+				"   Migrate actions: `up`, `upToLatest`, `down`",
+				"   Backup actions: `create`, `restore`",
+			);
+			return {
+				success: false,
+				error: undefined,
+			};
+		}
+
+		case "migrate": {
+			if (action != "up" && action != "upToLatest" && action != "down") {
+				return {
+					success: false,
+					error: "Invalid migrate action. Use `up`, `upToLatest`, or `down`.",
+				};
+			}
+
+			return {
+				success: true,
+				direction,
+				action,
+			};
+		}
+
+		case "backup": {
+			if (action != "create" && action != "restore") {
+				return {
+					success: false,
+					error: "Invalid backup action. Use `create` or `restore`.",
+				};
+			}
+
+			return {
+				success: true,
+				direction,
+				action,
+			};
+		}
+
+		default:
+			return {
+				success: false,
+				error: "Invalid command. Use `migrate` or `backup`.",
+			};
+	}
+}
